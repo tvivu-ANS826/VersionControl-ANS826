fab91aa06164f429acb1c7489007842319abe4bb
Vivien Tóth <tvivu.59@gmail.com>
2020-10-14 17:00:44 +0200

    haladás02

--- VAR-os/VAR-os/Entities/PortfolioItem.cs
+++ VAR-os/VAR-os/Entities/PortfolioItem.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace VAR_os.Entities
 {
-    class PortfolioItem
+    public class PortfolioItem
     {
+        public string Index { get; set; }
+        public decimal Volume { get; set; }
     }
 }
--- VAR-os/VAR-os/Form1.cs
+++ VAR-os/VAR-os/Form1.cs
@@ -15,12 +15,36 @@ namespace VAR_os
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
 
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
 
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+
+            PortfolioItem p = new PortfolioItem();
+            p.Index = "OTP";
+            p.Volume = 10;
+            Portfolio.Add(p);
+
+
+
+
+
+
         }
     }
 }
