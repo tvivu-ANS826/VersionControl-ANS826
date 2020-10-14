a822b9a26535d0108917acb0b87b731ed208561f
Vivien Tóth <tvivu.59@gmail.com>
2020-10-14 17:12:12 +0200

    haladás04

--- VAR-os/VAR-os/Form1.cs
+++ VAR-os/VAR-os/Form1.cs
@@ -41,33 +41,55 @@ namespace VAR_os
             Portfolio.Add(p);
 
 
-           /* int elemszám = Portfolio.Count();
-            
-            decimal részvényekSzáma = (from x in Portfolio select x.Volume).Sum();
-            MessageBox.Show(string.Format("Részvények száma: {0}", részvényekSzáma));
-            
-            DateTime minDátum = (from x in Ticks select x.TradingDay).Min();
+            /* int elemszám = Portfolio.Count();
 
-            DateTime maxDátum = (from x in Ticks select x.TradingDay).Max();
+             decimal részvényekSzáma = (from x in Portfolio select x.Volume).Sum();
+             MessageBox.Show(string.Format("Részvények száma: {0}", részvényekSzáma));
 
-            int elteltNapokSzáma = (maxDátum - minDátum).Days;
+             DateTime minDátum = (from x in Ticks select x.TradingDay).Min();
 
-            DateTime optMinDátum = (from x in Ticks where x.Index == "OTP" select x.TradingDay).Min();
+             DateTime maxDátum = (from x in Ticks select x.TradingDay).Max();
 
-            var kapcsolt =
-                from
-                    x in Ticks
-                         join
-              y in Portfolio
-                    on x.Index equals y.Index
-                select new
-                {
-                    Index = x.Index,
-                    Date = x.TradingDay,
-                    Value = x.Price,
-                    Volume = y.Volume
-                };
-            dataGridView1.DataSource = kapcsolt.ToList(); */
+             int elteltNapokSzáma = (maxDátum - minDátum).Days;
+
+             DateTime optMinDátum = (from x in Ticks where x.Index == "OTP" select x.TradingDay).Min();
+
+             var kapcsolt =
+                 from
+                     x in Ticks
+                          join
+               y in Portfolio
+                     on x.Index equals y.Index
+                 select new
+                 {
+                     Index = x.Index,
+                     Date = x.TradingDay,
+                     Value = x.Price,
+                     Volume = y.Volume
+                 };
+             dataGridView1.DataSource = kapcsolt.ToList(); */
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+
+
+
         }
 
         private decimal GetPortfolioValue(DateTime date)
@@ -83,6 +105,13 @@ namespace VAR_os
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+
+            
+           
+
         }
     }
+
+
 }
+
