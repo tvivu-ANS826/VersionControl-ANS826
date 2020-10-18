361d0ebcd2fa30a70b7985fcf2ad4fb077af0453
Vivien Tóth <tvivu.59@gmail.com>
2020-10-18 17:24:51 +0200

    haladás 03

---
+++ Webszolg/Webszolg/Entities/RateData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webszolg.MnbServiceReference;
+
+namespace Webszolg.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; set; }
+
+        public string Currency { get; set; }
+
+        public decimal Value { get; set; }
+
+
+
+    }
+}
--- Webszolg/Webszolg/Form1.cs
+++ Webszolg/Webszolg/Form1.cs
@@ -7,18 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolg.Entities;
 using Webszolg.MnbServiceReference;
 
 namespace Webszolg
 {
     public partial class Form1 : Form
     {
-        
+        RateData context = new RateData();
+        List<RateData> Rate;
+
 
 
         public Form1()
         {
             InitializeComponent();
+
+            Rate = context.Rate.ToList();
+            dataGridView1.DataSource = Rate;
 
             var mnbService = new MNBArfolyamServiceSoapClient();
 
