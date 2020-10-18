07154de0b23cebc2ac47187bc66e9bc8f8b7fe80
Vivien Tóth <tvivu.59@gmail.com>
2020-10-18 17:09:53 +0200

    haladás 02 ok

--- Webszolg/Webszolg/Form1.cs
+++ Webszolg/Webszolg/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolg.MnbServiceReference;
 
 namespace Webszolg
 {
     public partial class Form1 : Form
     {
+        
+
+
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
         }
     }
 }
