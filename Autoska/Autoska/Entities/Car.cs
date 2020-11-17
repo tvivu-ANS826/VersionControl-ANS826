77f4aec081e2f27eafa7efdd15af892492e87362
Vivien Tóth <tvivu.59@gmail.com>
2020-11-17 22:42:34 +0100

    next 06 fél

---
+++ Autoska/Autoska/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using Autoska.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoska.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
