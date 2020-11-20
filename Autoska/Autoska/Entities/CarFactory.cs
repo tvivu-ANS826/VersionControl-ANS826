2d935a3a5d8a978622e335c15f367a21a8d0e3b2
Vivien Tóth <tvivu.59@gmail.com>
2020-11-20 20:24:09 +0100

    next 08

--- Autoska/Autoska/Entities/CarFactory.cs
+++ Autoska/Autoska/Entities/CarFactory.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Autoska.Entities
 {
-   public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
