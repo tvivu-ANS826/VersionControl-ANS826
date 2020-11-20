a5a34e615fbdbbeeaa9ebb7f0d43602577613482
Vivien Tóth <tvivu.59@gmail.com>
2020-11-20 20:36:29 +0100

    next 09

--- Autoska/Autoska/Form1.cs
+++ Autoska/Autoska/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Autoska.Entities;
+using Autoska.Entities.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,11 @@ namespace Autoska
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        // private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
 
         private BallFactory _factory;
-        public BallFactory Factory
+        public BallFactory IToyFactory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -26,32 +28,32 @@ namespace Autoska
         public Form1()
         {
             InitializeComponent();
-            Factory = new BallFactory();
+            IToyFactory = new BallFactory();
         }
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = IToyFactory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var toy in _toys)
             {
-                ball.MoveBall();
-                if (ball.Left > maxPosition)
-                    maxPosition = ball.Left;
+                toy.MoveToy();
+                if (toy.Left > maxPosition)
+                    maxPosition = toy.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
     }
