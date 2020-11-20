cb4674efb9c39d0bec60e21e3ef631811c5ce7df
Vivien Tóth <tvivu.59@gmail.com>
2020-11-20 21:30:21 +0100

    next 12

---
+++ Autoska/Autoska/Entities/Present.cs
@@ -0,0 +1,18 @@
+﻿using Autoska.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoska.Entities
+{
+    public class Present : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
