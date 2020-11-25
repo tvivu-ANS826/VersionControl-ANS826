3d6a09d23d822d31c6a37935450ece937fd37cf2
Vivien Tóth <tvivu.59@gmail.com>
2020-11-25 19:20:29 +0100

    7-es kész

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -16,13 +16,40 @@ namespace UnitTestExample.Test
             TestCase("abcd1234", false),
             TestCase("irf@uni-corvinus", false),
             TestCase("irf.uni-corvinus.hu", false),
-            TestCase("irf@uni-corvinus.hu", true)
+            TestCase("irf@uni-corvinus.hu", true),
+            TestCase("0123456789",false)
+            
         ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             var accountController = new AccountController();
 
             var actualResult = accountController.ValidateEmail(email);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        //"A megadottt jelszó nem megfelelő!\n" +
+         //           "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, 
+        //és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
+
+
+
+        [
+            Test,
+            TestCase("AbcdefghLjhghgh@", false),
+            TestCase("ABCDEFG1", false),
+            TestCase("abcdefg1", false),
+            TestCase("ac1A",false),
+            TestCase("Abcdefg1", true),
+
+
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
 
             Assert.AreEqual(expectedResult, actualResult);
         }
