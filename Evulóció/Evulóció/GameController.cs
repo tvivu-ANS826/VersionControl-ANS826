e84c7bfb4430ae8fd429aa6f968fa99480f65adc
Vivien Tóth <tvivu.59@gmail.com>
2020-12-01 20:39:14 +0100

    1-2 feladat készen van

---
+++ Evulóció/Evulóció/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldsHardestGame;
+
+namespace Evulóció
+{
+    public class GameController
+    {
+        internal GameArea ActivateDisplay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
